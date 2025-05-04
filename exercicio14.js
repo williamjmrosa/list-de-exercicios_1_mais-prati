9602f71bb37589d1fa0db566a542312ef5755dfe
William José <williamjmrosa@gmail.com>
2025-05-04 20:12:03 -0300

    fatorial usando função recursiva

--- exercicio14.js
+++ exercicio14.js
@@ -4,10 +4,17 @@
 const prompt = require("prompt-sync")()
 
 let numero = Number(prompt('Digite um número para calcular o fatorial:'))
-let fatorial = 1
 
-for (let i = numero; i != 0; i--){
-    fatorial = fatorial * i
+function fatorialFuncao(numero){
+    if(numero == 0 || numero == 1){
+        return 1
+    }else{
+        return numero * fatorialFuncao(numero - 1)
+    }
 }
 
-console.log("O fatorial de " + numero + " é: " + fatorial)
+if(Number.isInteger(numero) == false || numero < 0){
+    console.log("O número digitado é inválido.")
+}else{
+    console.log("O fatorial de " + numero + " é: " + fatorialFuncao(numero))
+}
