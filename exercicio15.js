f90d328678afcbeb6434540a8659d9e21f881210
William José <williamjmrosa@gmail.com>
2025-05-20 18:58:49 -0300

    Melhoria para mostrar

--- exercicio15.js
+++ exercicio15.js
@@ -2,10 +2,8 @@
 // Fibonacci utilizando um loop for.
 
 let fibonacci = [0, 1]
-let sequência = "0, 1"
 
 for (let i = 2; i < 10; i++){
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
-    sequência += ", " + fibonacci[i]
 }
-console.log("Os primeiros 10 números da sequência de Fibonacci são:", sequência)
+console.log("Os primeiros 10 números da sequência de Fibonacci são:", fibonacci.join())
